666468dd7298284217040f731630fe8ca37bfde5
Valerianavo <lilaj2sy@gmail.com>
2024-08-16 21:03:35 -0500

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,53 @@
-// Aqui escribire mi archivo javascript
-// Desde aqui comienzan mis pruebas 
+function validarTarjeta(){
+    let alerta = document.getElementById("alerta");
+    alerta.innerHTML="";
+    alerta.className="";
+
+
+
+
+    let numero = document.getElementById("numero-tarjeta");
+    let digitosTC = numero.value;
+    
+    let arrTC = Array.from(digitosTC);
+    console.log(arrTC);
+    let arrInverso = arrTC.reverse();
+    //console.log(arrInverso);
+    let digitoPar = -1;
+    for (let i = 1; i <= arrInverso.length; i+=2){
+        digitoPar = parseInt(arrInverso[i]);
+        digitoPar = digitoPar*2;
+        //console.log(digitoPar);
+        if(digitoPar>=10){
+            digitoPar = digitoPar.toString();
+            //console.log(digitoPar);
+            digitoPar = parseInt(digitoPar[0])+parseInt(digitoPar[1]);
+            //console.log(digitoPar);
+        }
+        arrInverso[i] = digitoPar.toString();
+    }
+    console.log(arrInverso);
+
+    let suma = 0;
+    for (let j = 0; j < arrInverso.length; j++) {
+    suma = suma + parseInt(arrInverso[j]);
+    }
+    console.log(suma);
+
+    if(suma%10==0){
+        alerta.innerHTML = "Tarjeta Valida";
+        alerta.className = "alerta-valida";
+    } else{
+        alerta.innerHTML = "Tarjeta Inavalida";
+        alerta.className = "alerta-error";
+    }
+
+}
+
+
+
+
+
+document.getElementById("btn-validar").addEventListener("click", function(){
+    validarTarjeta();
+})
